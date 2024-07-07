9d1f75e058cafaea8c31cbcde7611c576ce9435f
Polina Minina <115030427+polinaMinina@users.noreply.github.com>
2024-07-07 11:12:33 +0300

    Update update_documents.py

--- update_documents.py
+++ update_documents.py
@@ -3,7 +3,8 @@ import os
 from update_docs.split_docs import split
 from update_docs.parse_json import parse
 from update_docs.update import update_indexes
-from update_docs.parsser import parse_rustore
+# Расскомментировать для запуска парсинга
+# from update_docs.parsser import parse_rustore
 
 def load_page2():
     st.title("Обновление документации")
@@ -11,9 +12,10 @@ def load_page2():
 
     # Button to update documents
     if st.button("Обновить документацию"):
-        with st.spinner("Парсим документацию...")
-            parse_rustore()
-        st.succes("Parsing complete succesfully.")
+        # Расскомментировать для запуска парсинга
+        # with st.spinner("Парсим документацию...")
+        #     parse_rustore()
+        # st.succes("Parsing complete succesfully.")
         with st.spinner("Обрабатываем JSON файлы..."):
             parse()
         st.success("Documents updated successfully.")
